662f806234decdc0a7aa68bcd16259c8aad5ec21
Whtcorps Inc <71530574+whtcorpsinc@users.noreply.github.com>
2021-07-31 10:51:21 -0700

    tons of content
    jus twzr

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,8 +22,11 @@ const HomePage = () => {
       <Banner />
       <TextBlock
         id="about"
-        title="Developing a new business is never easy, but Startup is here to help"
-        paragraph="With a super-fast Gatsby powered website you have the perfect platform to get your idea off the ground. Its the perfect template to help turn your idea into a reality."
+        title="EinsteinDB introduces the InterLock that extends
+a pessimistic RW-Mutex with support for optimistic locking"
+        paragraph=" combining all fields of both locks
+into a single 64-bit word to keep
+the overhead and latency of locking minimal. "
       >
         <Link to="perks" smooth={true} duration={500}>
           <Button label="Tell Me More" cta="Tell Me More!" />
@@ -31,22 +34,31 @@ const HomePage = () => {
       </TextBlock>
       <TextBlockImg
         id="perks"
-        title="World's First Relativistic Linearizable PR2SQL Hybrid Online and Analytical Procwessing Engine"
+        title="World's First Relativistic Linearizable PR2SQL Hybrid Online and Analytical Processing Engine for embedded devices and data centres alike "
         subtitle="Exhaustive Causal Consistent network simulation combined with lineage driven concurrency testing of lock-free algorithm interleavings, 
-        model-based testing in combination with fault injection, leaderless byzantine lamport invariant consensus"
+        model-based testing in combination with fault injection, leaderless byzantine lamport invariant consensus separates MilevaDB's  BerolinaSQL layer (the 
+          MilevsDB servers) from MilevaDB's storage engine (the EinsteinDB servers, with data in event2vec format). 
+          Instead of tightly coupling the two formats in one engine, we chose the VioletaBFT algorithm to split-fuse event and scheme This 
+          loosely-coupled approach has the advantage of serializability"
       >
         <div className="flex-container trio-block">
           <Perk
             img={perk1Img}
             alt=" cloud-native stochastic semi-relational database compatible with MySQL, InnoDB, MariaDB, PostgreSQL, SQLite and Oracle"
-            title="enterprise database"
-            content="EinsteinDB’s Causet Secondary Index averages a response time less than 1 ms (P99=10 ms). EinsteinDB is a multi-version, globally-distributed, leader-less, and synchronously-replicated database written with rust, golang, haskell, and C"
+            title="VioletaBFT'slogs preserved all details required for data replication ---"
+            content="One of EinsteinDB's biggest strengths is its HTAP architecture. In MilevaDB and EinsteinDB, the row(event) store and column store(causet_state=timelike_n_0) can coexist 
+            and provide service for different kinds of queries.
+            EinsteinDB’s Causet Secondary Index averages a response time less than 1 ms (P99=10 ms). 
+            EinsteinDB is a multi-version, globally-distributed, 
+            leader-less, and synchronously-replicated database written with rust, golang, haskell, and C"
           />
           <Perk
             img={perk2Img}
-            alt="Our implementation of Paxos is pipelined between HoneyBadger BFT and Byzantine Raft, so as to improve EinsteinDB's throughput in the presence of WAN latencies."
-            title="With an improved database kernelm, EinsteinDB, supports physical replication, RDMA protocol, and shared distributed storage, which greatly improves performance."
-            content="A choice of packages to suit every business type and size"
+            alt="Our implementation of Byzantine consensus via HoneyBadger meets Tangaroa: Paxos is pipelined between HoneyBadger BFT and Byzantine Raft, so as to improve EinsteinDB's throughput in the presence of WAN latencies."
+            title="With an improved database kernel, EinsteinDB, now supports physical replication, RDMA protocol, and shared distributed storage, which greatly improves performance using a learned key-value dictionary on SmartNIC."
+            content=". Practical byzantine fault tolerance (PBFT) that assumes a weak synchronous network
+          model was the first practical protocol to be applied to a
+            real system"
           />
           <Perk
             img={perk3Img}
@@ -101,7 +113,7 @@ MilevaDB is an open-source, distributed Prolog and sSQL database, and EinsteinDB
               </li>
               <li>
                 <MdDone />
-                ransactions in MilevaDB use two-phase commit (2PC) that includes the Prewrite phase and the Commit phase .
+                Transactions in MilevaDB use two-phase commit (2PC) that includes the Prewrite phase and the Commit phase .
               </li>
             </ul>
             <Link to="contact" smooth={true} duration={500}>
@@ -120,11 +132,13 @@ MilevaDB is an open-source, distributed Prolog and sSQL database, and EinsteinDB
               </li>
               <li>
                 <MdDone />
-              MilevaDB supports the JSON (JavaScript Object Notation) data type, which is useful for storing semi-structured data. 
+              MilevaDB supports the JSON (JavaScript Object Notation) data type, which is useful for storing semi-structured data. We first use cost as feedback to train the model and then switch to latency as feedback for fine-tuning. 
+              Different from that only uses latency as feedback, we treat these two feedback as two separate tasks but sharing one common representation, through multi-task learning.
               </li>
               <li className="linethrough">
                 <MdClear color="red" />
                 A view acts as a virtual table, whose schema is defined by the SELECT statement that creates the view. 
+                 By doing so, the model can learn cost and latency together, by setting the objective loss function as a weighted sum of these two problems.
               </li>
             </ul>
             <Link to="contact" smooth={true} duration={500}>
@@ -135,8 +149,9 @@ MilevaDB is an open-source, distributed Prolog and sSQL database, and EinsteinDB
       </Packages>
       <Contact
         id="contact"
-        title="Contact Startup today and see how we can help your business grow"
-        subtitle="Every second counts when you're looking to get your new business started. Drop Startup a message and one of our representatives will be in contact"
+        title="the new standard for processing data and responding to an event is now under 250 milliseconds."
+        subtitle=" That means that the decisioning part must happen in single-digit milliseconds (<10), without compromising data accuracy. 
+        Every second counts when you're looking to get your new business started. Drop Startup a message and one of our representatives will be in contact"
       />
     </>
   )
